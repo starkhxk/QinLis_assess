973a7116256f8edd0a3e936cccf54ba690b04ed3
wangenze <244169455@qq.com>
2019-12-04 12:51:04 +0800

    wangenze

--- 考核/second/002.js
+++ 考核/second/002.js
@@ -1,75 +1,33 @@
-/*function validateForm() {
-    var x = document.forms["myform"]["StudentID"].value;
-    if (x == "") {
-        alert("必须填写学号！");
-        return false;
-    }
-    var y = document.forms["myform"]["IDnumber"].value;
+var json = [{
+    "StudentID": "1903160122",
+    "IDnumber": "210682200205110012",
+    "name": "王恩泽",
+    "sex": "1"
+}, {
+    "StudentID": "1903160125",
+    "IDnumber": "412824200201133910",
+    "name": "郭伟凡",
+    "sex": "1"
+}]
 
-    if (y == "") {
-        alert("必须填写身份证号！");
-        return false;
-    }
-    var z = document.forms["myform"]["name"].value;
-    if (z == "") {
-        alert("必须填写姓名！");
-        return false;
-    }
-}*/
-//检查填写是否为空
-function validateForm() {
-    var x = document.forms["myform"]["StudentID"].value;
-    var y = document.forms["myform"]["IDnumber"].value;
-    var z = document.forms["myform"]["name"].value;
-    if (x == "") {
-        alert("必须填写学号！");
-        return false;
-    } else {
-        if (y == "") {
-            alert("必须填写身份证号！");
-            return false;
-        } else {
-            if (z == "") {
-                alert("必须填写姓名！");
-                return false;
-            }
-        }
-    }
-}
-
-function Esempty() {
-
-    if (document.getElementById("sex").checked) {
-        return true;
-    } else {
-        alert("请选择您的性别！");
-
-        return false;
-    }
-
-}
-var arr = [
-
-
-    { "StudentID1": "1903160122", "IDnumber1": "210682200205110012", "name1": "王恩泽" },
-    { "StudentID2": "1903160125", "IDnumber2": "412824200201133910", "name2": "郭伟凡" }
-
-]
 
 function checked() {
-    if (StudentID.value == arr[0].StudentID1 && IDnumber1 && name1) {
-        alert("注册成功！");
-        return true;
-    } else {
-        if (StudentID.value == arr[1].StudentID2 && IDnumber2 && name2) {
-            alert("注册成功！");
-            return true;
+    var StudentID = document.getElementById("StudentID").value;
+    var IDnumber = document.getElementById("IDnumber").value;
+    var name = document.getElementById("name").value;
+    var nan = document.getElementById("nan");
+    var nv = document.getElementById("nv");
 
-
+    for (var i = 0; i < json.length; i++) {
+        if (StudentID == json[i].StudentID && IDnumber == json[i].IDnumber && name == json[i].name) {
+            if ((nan.checked)) {
+                alert("注册成功！");
+                return true;
+            }
         } else {
-            alert("您输入的是无效信息！");
+            alert("您提交的信息不正确，请核对您的信息!");
             return false;
-
         }
+
     }
 }
